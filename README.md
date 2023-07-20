b532cf5031dc37fc7553d3cfdcb520437fae7f77
Hamza <e.hamza@bbnd.eu>
2023-07-20 13:30:55 +0200

    new version 2.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-![Trace your mail](https://checkup.email/assets/img/logo.png)
+![Trace your mail](https://abada.bbnd.eu/assets/img/logo.png)
 # Abada ✉️
-Verify an email address if it's pwned or not.
+is a Python-based tool that allows you to check if an email address has been compromised in a data breach. It utilizes the BreachDirectory API from RapidAPI to provide comprehensive information about the breach, if any.
 
 ⛔If the tool is used for malicious purposes, we will not be liable.⛔
 
@@ -10,34 +10,57 @@ Verify an email address if it's pwned or not.
 - **Multitasking tool**
 - **Instant answer**
 
+## Features
+- Quick and easy verification of email addresses for data breaches.
+- Utilizes the BreachDirectory API to fetch breach information.
+- Provides detailed breach information including breach name, date, and affected accounts.
+
 ## Install
+
+Clone the repository
 
 ```sh
 $ git clone https://github.com/BBND/Abada
 $ cd Abada
 ```
 
+**Obtain an API key from RapidAPI (BreachDirectory API) here :**
+https://rapidapi.com/rohan-patra/api/breachdirectory
+
 ## How it works ?
 
 | Argument | Description |
 | ------ | ------ |
 | -h, --help | Display help message and exit |
-| -e [address], --email [address] | Put an email to check if it's leaked or not |
-| -f [file], --filename [file]  | Put a file to check all addresses per line |
-| --proxy [ip:port]  | Use a proxy |
+| -e [address], --email [address] | Put your email address |
+| -k [key], --key [key] | Put your RapidApi key |
+
 
 ## Usage Examples
 
-```sh
-$ python abada.py -e test@example.com
-```
+To verify an email address, follow these steps:
+
+- Open the terminal and navigate to the project directory.
+- Run the script with the following command:
 
 ```sh
-$ python abada.py -e test@example.com --proxy[0.0.0.0:99]
+$ python abada.py -e test@example.com -k your_key
 ```
+
+- Enter the email address you want to verify when prompted, and enter the API key.
+- The tool will connect to the BreachDirectory API and retrieve the breach information for the provided email address(es).
+- The results will be displayed on the console, showing the breach name, date, and affected accounts, if any.
+
+### Data Deletion
+
+If you are the owner of an email address and find that your email has been compromised in a breach, you have the option to request the removal of your data from the BreachDirectory database. You can do this by visiting the [Delete My Data](https://breachdirectory.org/deletemydata) page and following the instructions provided there.
+
+## Disclaimer
+
+This tool is provided for educational and informational purposes only. The accuracy and completeness of the data returned by the BreachDirectory API may vary. Use the tool responsibly and respect the privacy of others.
 
 ## Author
 [BBND](https://www.bbnd.eu)
 
 ## For more informations :
-[Checkup email](https://www.checkup.email)
+[ABADA](https://abada.bbnd.eu)
--- abada.py
+++ abada.py
@@ -1,301 +1,119 @@
 import argparse
-
 import requests
-
 import time
-
 import sys
-
 import json
 
-
-
 OK = '\033[92m'
-
 WARNING = '\033[93m'
-
 FAILRED = '\033[91m'
-
 ENDLINE = '\033[0m'
 
 
-
-
-
 def print_table(iterable, header):
-
     max_len = [len(x) for x in header]
-
     for row in iterable:
-
         row = [row] if type(row) not in (list, tuple) else row
-
         for index, col in enumerate(row):
-
             if max_len[index] < len(str(col)):
-
                 max_len[index] = len(str(col))
-
     output = '-' * (sum(max_len) + 1) + '\n'
-
     output += '|' + ''.join([h + ' ' * (l - len(h)) + '|' for h, l in zip(header, max_len)]) + '\n'
-
     output += '-' * (sum(max_len) + 1) + '\n'
-
     for row in iterable:
-
         row = [row] if type(row) not in (list, tuple) else row
-
         output += '|' + ''.join([str(c) + ' ' * (l - len(str(c))) + '|' for c, l in zip(row, max_len)]) + '\n'
-
     output += '-' * (sum(max_len) + 1) + '\n'
 
     return output
 
 
-
-
-
-def consult(email, proxy):
-
-    try:
-
-        user_agent = {"User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion", }
-
-        if proxy is "None":
-
-            r = requests.get('https://haveibeenpwned.com/api/v2/breachedaccount/' + email, headers=user_agent)
-
-        else:
-
-            proxy_object = {'http': 'http://' + proxy}
-
-            r = requests.get('https://haveibeenpwned.com/api/v2/breachedaccount/' + email, headers=user_agent,
-
-                             proxies=proxy_object)
-
-
-
-        if r.status_code == 200:
-
+def consult(email, key):
+    url = "https://breachdirectory.p.rapidapi.com/"
+    body = {"func":"auto","term":email}
+    headers = {
+        "X-RapidAPI-Key": key,
+        "X-RapidAPI-Host": "breachdirectory.p.rapidapi.com"
+    }
+    r = requests.get(url, headers=headers, params=body)
+    results = r.json()
+    if r.status_code == 200:
+        if results['result']:
             print(FAILRED + "[!!] Your email " + email + " has been leaked" + ENDLINE)
-
             print("Breaches you were pwned in :")
-
-            json_content = r.content.decode('utf8')
-
-            jsonn = json.loads(json_content)
-
             table = []
-
-            header = ['Website', 'Date', 'Emails leaked']
-
-            for j in jsonn:
-
-                domain = j['Domain']
-
-                breachDate = j['BreachDate']
-
-                pwnCount = j['PwnCount']
-
-                table.append([domain, breachDate, pwnCount])
-
+            header = ['Sources', 'Password', 'Date']
+            for row in results['result']:
+                sources = ','.join(row['sources'])
+                password = row['line'].split(':')[1] if not row['email_only'] else 'no_password'
+                date_beach = row['last_breach']
+                table.append([sources, password, date_beach])
             x = print_table(table, header)
-
             loading()
-
             print('\n')
-
             print(x)
-
-
-
-        elif r.status_code == 404:
-
-            print(OK + "[i] Your email " + email + " has not been leaked" + ENDLINE)
-
-
-
-        elif r.status_code == 400:
-
-            print(WARNING + "[!] Please retry another email ! " + ENDLINE)
-
-
-
-        elif r.status_code == 429:
-
-            print(
-
-                WARNING + "[!] Too many request, please retry after " + r.headers['Retry-After'] + " seconds" + ENDLINE)
-
-            time_to_retry = float(r.headers['Retry-After'])
-
-            time.sleep(time_to_retry)
-
-            consult(email)
-
-        elif r.status_code == 403:
-
-            print(' 403')
-
-
-
         else:
-
-            print(WARNING + "[!] Something is wrong, please retry again" + ENDLINE)
-
-    except:
-
-        print("Some error occurred with queries leakeds.")
-
-
-
+            print(OK + "[i] Your email " + email + " has not been leaked" + ENDLINE)
+    else :
+        print(WARNING + "[!] Something is wrong, please verify your api key or retry again" + ENDLINE)
+    
 
 
 def banner():
-
     print("=======================================================")
-
     print("\033[91m \033[1m _______  _______  _______  ______   _______ \033[0m")
-
     print("\033[91m \033[1m|   _   ||  _    ||   _   ||      | |   _   |\033[0m")
-
     print("\033[91m \033[1m|  |_|  || |_|   ||  |_|  ||  _    ||  |_|  |\033[0m")
-
     print("\033[91m \033[1m|       ||       ||       || | |   ||       |\033[0m")
-
     print("\033[91m \033[1m|       ||  _   | |       || |_|   ||       |\033[0m")
-
     print("\033[91m \033[1m|   _   || |_|   ||   _   ||       ||   _   |\033[0m")
-
-    print("\033[91m \033[1m|__| |__||_______||__| |__||______| |__| |__|\033[0m v1.0 \n")
-
-
-
+    print("\033[91m \033[1m|__| |__||_______||__| |__||______| |__| |__|\033[0m v2.0 \n")
 
 
 def menu():
-
     print("=======================================================\n")
-
-    print("Usage : This tool verify if the email is leaked or not.\n")
-
+    print("ABADA is a Python script that allows you to check if an email address has been compromised in any known data breaches.\n")
+    print("It provides a simple and convenient way to verify the security of email addresses and gain insights into potential risks.\n")
     print("=======================================================\n")
-
-
-
 
 
 def anim():
-
     array = ['[]']
-
     for i in range(1, 61):
-
         text = ''
-
         for j in range(i):
-
             text = text + '#'
-
         array.append('[' + text + ']')
-
-
-
     return array
 
 
-
-
-
 def loading():
-
     animation = anim()
-
-
-
     for i in range(1, 61):
-
         time.sleep(0.1)
-
         sys.stdout.write("\r" + "Loading : " + animation[i % len(animation)] + ' ' + str(int(round(i * 5 / 3))) + '%')
-
         sys.stdout.flush()
 
 
-
-
-
-def main(email, filename, proxy):
-
-    if email != "None":
-
-        loading()
-
-        print("\n")
-
-        consult(email, proxy)
-
-    else:
-
-        emails = [line.rstrip('\n') for line in open(filename)]
-
-        loading()
-
-        print("\n")
-
-        for email in emails:
-
-            consult(email, proxy)
-
-
-
+def main(email, key):
+    loading()
+    print("\n")
+    consult(email, key)
 
 
 if __name__ == '__main__':
-
-
-
     banner()
-
     menu()
-
-
-
     parsing = argparse.ArgumentParser(description="This tool verify if the email is leaked or not.")
-
-
-
-    parsing.add_argument('-e', '--email', dest='email', help='Insert your email')
-
-    parsing.add_argument('-f', '--file', dest='filename', help="File to check all addresses per line")
-
-    parsing.add_argument('--proxy', dest='proxy', help="Put a proxy")
-
+    parsing.add_argument('-e', '--email', dest='email', help='Put your email')
+    parsing.add_argument('-k', '--key', dest='key', help="Put your RapidApi key")
     args = parsing.parse_args()
-
-
-
     email = str(args.email)
+    key = str(args.key)
 
-    filename = str(args.filename)
-
-    proxy = str(args.proxy)
-
-
-
-    if email == "None" and filename == "None":
-
-        print('Options : ')
-
-        print('	-e, --email		[address]	Put an email to check if its leaked or not.\n')
-
-        print('	-f, --filename	[file]		Put a file to check all addresses per line\n')
-
-        print('	--proxy			[ip:port]	Put a proxy\n')
-
-        print('	-h , --help					Show this help message and exit\n')
-
+    if email == "None":
+        print(FAILRED + '!!! Please put an email address' + ENDLINE)
+    elif key == "None":
+        print(FAILRED + '!!! Please put RapidApi key' + ENDLINE)
     else:
-
-        main(email, filename, proxy)
+        main(email, key)
